3cc4c94e5a642bd82d073005a4ce227dc59afd34
Miguel Hernandez <MiguelAlejandro.HernandezZarasu@aa.com>
2025-09-01 15:45:39 -0600

    testing ngIf

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,9 +1,10 @@
 import { Course } from './../model/course';
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'course-card',
-  imports: [],
+  imports: [CommonModule ],
   templateUrl: './course-card.component.html',
   styleUrl: './course-card.component.css'
 })
@@ -17,6 +18,10 @@ export class CourseCardComponent {
 
   @Output()
   courseSelected = new EventEmitter<Course>();
+
+  isImageVisible(){
+    return this.course && this.course.iconUrl;
+  }
 
   constructor(){ }
 
